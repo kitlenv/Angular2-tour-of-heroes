429b29ef3c4deaa49b5380e7df3dd0f16b203456
Sam Wang <samwang@Sams-iMac.BigPond>
2016-05-29 00:43:23 +1000

    update

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import { HeroService } from './hero.service';
 import { HeroesComponent } from './heroes.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
@@ -31,6 +32,11 @@ import { DashboardComponent } from './dashboard.component';
         name: 'Dashboard',
         component: DashboardComponent,
         useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
     }
 ])
 export class AppComponent{
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router-deprecated';
 
 @Component({
     selector: 'my-dashboard',
@@ -10,13 +11,17 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit{
     heroes: Hero[] = [];
     
-    constructor(private heroService: HeroService){}
+    constructor(private heroService: HeroService,
+        private router: Router){}
     
     ngOnInit(){
         this.heroService.getHeroes()
         .then(heroes => this.heroes = heroes.slice(1,5));
     }
     
-    gotoDetail(hero: Hero){}
+    gotoDetail(hero: Hero){
+        let link = ['HeroDetail', {id: hero.id}];
+        this.router.navigate(link);
+    }
     
 }
